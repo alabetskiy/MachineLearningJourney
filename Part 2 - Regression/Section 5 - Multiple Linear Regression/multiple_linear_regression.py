322d98c773d6ca999ef4635493c871a071208969
alabetskiy <labetskiy@gmail.com>
2018-04-28 20:56:11 -0400

    Multiple Linear Regression

---
+++ Part 2 - Regression/Section 5 - Multiple Linear Regression/multiple_linear_regression.py
@@ -0,0 +1,42 @@
+
+# Importing the libraries
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+
+# Importing the dataset
+dataset = pd.read_csv('50_Startups.csv')
+X = dataset.iloc[:, :-1].values
+y = dataset.iloc[:, 4].values
+
+# Encoding categorical data
+# Encoding the Independent Variable
+from sklearn.preprocessing import LabelEncoder, OneHotEncoder
+labelencoder_X = LabelEncoder()
+X[:, 3] = labelencoder_X.fit_transform(X[:, 3])  # we need to use labelencoder first, because we have to have numbers instead string values, than use onehotencoder
+onehotencoder = OneHotEncoder(categorical_features = [3]) # 3 index in categorical_features means which column to group
+X = onehotencoder.fit_transform(X).toarray()
+
+#Avoidingthe Dummy Varibale Trap
+# we don't need to remove it, because Linear Regression takes care of it. But there are some libraries which don't do that
+X = X[:, 1:] #remove one first column
+
+# Splitting the dataset into the Training set and Test set
+from sklearn.cross_validation import train_test_split
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.2, random_state = 0)
+
+#We don't need to apply Feature Scalling because library will apply it for us
+
+# Fitting Mulpile Linear Regression to the Training Set
+from sklearn.linear_model import LinearRegression
+regressor = LinearRegression()
+regressor.fit(X_train, y_train)
+
+# Predict the Test set results
+y_pred = regressor.predict(X_test)
+
+#Building the optimal model using Backward Elimination 
+import statsmodels.formula.api as sm
+X = np.append(arr = np.ones((50,1)).astype(int), values = X, axis = 1)
+X_opt = X[:, [0, 1, 2, 3, 4, 5]]       #X_opt will contain only variables which have significant impact on model prediction. 
+regressor_OLS = sm.OLS()
