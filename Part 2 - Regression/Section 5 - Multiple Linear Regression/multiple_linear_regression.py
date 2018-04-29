a91c24f84d576de2321db7f1240e3bfd553ed70e
alabetskiy <labetskiy@gmail.com>
2018-04-28 22:57:43 -0400

    Logistic Regression

--- Part 2 - Regression/Section 5 - Multiple Linear Regression/multiple_linear_regression.py
+++ Part 2 - Regression/Section 5 - Multiple Linear Regression/multiple_linear_regression.py
@@ -39,4 +39,32 @@ y_pred = regressor.predict(X_test)
 import statsmodels.formula.api as sm
 X = np.append(arr = np.ones((50,1)).astype(int), values = X, axis = 1)
 X_opt = X[:, [0, 1, 2, 3, 4, 5]]       #X_opt will contain only variables which have significant impact on model prediction. 
-regressor_OLS = sm.OLS()
+regressor_OLS = sm.OLS(endog = y, exog = X_opt).fit()
+regressor_OLS.summary()
+
+X_opt = X[:, [0, 1, 3, 4, 5]]       #X_opt will contain only variables which have significant impact on model prediction. 
+regressor_OLS = sm.OLS(endog = y, exog = X_opt).fit()
+regressor_OLS.summary()
+
+X_opt = X[:, [0, 3, 4, 5]]       #X_opt will contain only variables which have significant impact on model prediction. 
+regressor_OLS = sm.OLS(endog = y, exog = X_opt).fit()
+regressor_OLS.summary()
+
+X_opt = X[:, [0, 3, 5]]       #X_opt will contain only variables which have significant impact on model prediction. 
+regressor_OLS = sm.OLS(endog = y, exog = X_opt).fit()
+regressor_OLS.summary()
+
+X_opt = X[:, [0, 3]]       #X_opt will contain only variables which have significant impact on model prediction. 
+regressor_OLS = sm.OLS(endog = y, exog = X_opt).fit()
+regressor_OLS.summary()
+
+
+
+from sklearn.cross_validation import train_test_split
+X_train, X_test, y_train, y_test = train_test_split(X_opt, y, test_size = 0.2, random_state = 0)
+
+regressor = LinearRegression()
+regressor.fit(X_train, y_train)
+
+# Predict the Test set results
+y_pred = regressor.predict(X_test)
