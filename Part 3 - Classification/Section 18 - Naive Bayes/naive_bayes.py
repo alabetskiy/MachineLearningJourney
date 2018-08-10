153a4c8a8b5709eb02fcbb80319a116eeef06063
alabetskiy <labetskiy@gmail.com>
2018-08-10 19:32:31 -0400

    Naive Bayes

--- Part 3 - Classification/Section 18 - Naive Bayes/naive_bayes.py
+++ Part 3 - Classification/Section 18 - Naive Bayes/naive_bayes.py
@@ -18,11 +18,10 @@ sc_X = StandardScaler()
 X_train = sc_X.fit_transform(X_train)
 X_test = sc_X.transform(X_test)
 
-# Fitting the SVM classifier to the Training Set
-from sklearn.svm import SVC
-classifier = SVC(kernel = 'poly', random_state = 0)
+# Fitting the classifier to the Training Set
+from sklearn.naive_bayes import GaussianNB
+classifier = GaussianNB()
 classifier.fit(X_train, y_train)
-
 
 # Predict Test set results
 y_pred = classifier.predict(X_test)
@@ -30,24 +29,6 @@ y_pred = classifier.predict(X_test)
 # Making the Confusion Matrix 
 from sklearn.metrics import confusion_matrix
 cm = confusion_matrix(y_test, y_pred)
-
-# Visualising the Training set results
-from matplotlib.colors import ListedColormap
-X_set, y_set = X_train, y_train
-X1, X2 = np.meshgrid(np.arange(start = X_set[:, 0].min() - 1, stop = X_set[:, 0].max() + 1, step = 0.005),
-                     np.arange(start = X_set[:, 1].min() - 1, stop = X_set[:, 1].max() + 1, step = 0.005))
-plt.contourf(X1, X2, classifier.predict(np.array([X1.ravel(), X2.ravel()]).T).reshape(X1.shape),
-             alpha = 0.75, cmap = ListedColormap(('red', 'green')))
-plt.xlim(X1.min(), X1.max())
-plt.ylim(X2.min(), X2.max())
-for i, j in enumerate(np.unique(y_set)):
-    plt.scatter(X_set[y_set == j, 0], X_set[y_set == j, 1], edgecolors='black',
-                c = ListedColormap(('red', 'green'))(i), label = j)
-plt.title('Support Vector Machine (Train set)')
-plt.xlabel('Age')
-plt.ylabel('Estimated Salary')
-plt.legend()
-plt.show()
 
 # Visualising the Test set results
 from matplotlib.colors import ListedColormap
@@ -61,7 +42,7 @@ plt.ylim(X2.min(), X2.max())
 for i, j in enumerate(np.unique(y_set)):
     plt.scatter(X_set[y_set == j, 0], X_set[y_set == j, 1], edgecolors='black',
                 c = ListedColormap(('red', 'green'))(i), label = j)
-plt.title('Support Vector Machine (Test set)')
+plt.title('Naive Bayes (Train set)')
 plt.xlabel('Age')
 plt.ylabel('Estimated Salary')
 plt.legend()
